7ace8983951cf83d1b3e95d80b2ba1c61d3526ff
Lorenzo <lorenzooliverio12@gmail.com>
2024-05-19 03:40:59 -0300

    Ejercicio3Realizado

---
+++ 3..Ejercicio/Calculadora/Operaciones.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Calculadora;
+public class Metodos
+{
+    public int Num1 { get; set; }
+    public int Num2 { get; set; }
+
+    public void Numeros()
+    {
+        Console.WriteLine("Ingrese el primer número:");
+        Num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número:");
+        Num2 = int.Parse(Console.ReadLine());
+    }
+
+    public string Suma(int Num1, int Num2)
+    {
+        return "El Resultado es: " + (Num1 + Num2);
+    }
+
+    public string Resta(int Num1, int Num2)
+    {
+        
+        return "El Resultado es: " + (Num1 - Num2);
+    }
+
+    public string Multiplicación(int Num1, int Num2)
+    {
+        return "El Resultado es: " + (Num1 * Num2);
+    }
+
+    public string División(int Num1, int Num2)
+    {
+        if (Num2 == 0)
+        {
+            return "No se puede dividir por 0";
+        }
+        else 
+        {
+            return "El Resultado es: " + (Num1 / Num2);
+        }
+        
+    }
+    
+}
---
+++ 3..Ejercicio/Calculadora/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Calculadora;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Metodos metodos = new Metodos();
+        
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Programa de la calculadora");
+            Console.WriteLine("Seleccione que operacion quiera realizar");
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Multiplicación");
+            Console.WriteLine("4. División");
+            Console.WriteLine("5. Salir");
+            string operacion = Console.ReadLine();
+
+            if (operacion == "5")
+            {
+                Console.Clear();
+                Console.WriteLine("Saliendo del programa");
+                Console.WriteLine("Presione cualquier tecla para cerrar");
+                Console.ReadKey();
+                break;
+            }
+            switch (operacion)
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("Operacion seleccionada: Suma");
+                    metodos.Numeros();
+                    Console.WriteLine(metodos.Suma(metodos.Num1, metodos.Num2));
+                    Console.WriteLine("Presione una tecla para continuar");
+                    Console.ReadKey();
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("Operacion seleccionada: Resta");
+                    metodos.Numeros();
+                    Console.WriteLine(metodos.Resta(metodos.Num1, metodos.Num2));
+                    Console.WriteLine("Presione una tecla para continuar");
+                    Console.ReadKey();
+                    break;
+                case "3":
+                    Console.Clear();
+                    Console.WriteLine("Operacion seleccionada: Multiplicación");
+                    metodos.Numeros();
+                    Console.WriteLine(metodos.Multiplicación(metodos.Num1, metodos.Num2));
+                    Console.WriteLine("Presione una tecla para continuar");
+                    Console.ReadKey();
+                    break;
+                case "4":
+                    Console.Clear();
+                    Console.WriteLine("Operacion seleccionada: División");
+                    metodos.Numeros();
+                    Console.WriteLine(metodos.División(metodos.Num1, metodos.Num2));
+                    Console.WriteLine("Presione una tecla para continuar");
+                    Console.ReadKey();
+                    break;      
+                default:
+                    Console.WriteLine("Operacion no valida");
+                    break;
+
+            }
+        }               
+    }
+}
